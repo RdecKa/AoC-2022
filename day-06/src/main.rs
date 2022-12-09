9632afa4455be08b89b6dd5592c7653b674f1631
Karin Frlic <karin.frlic@gmail.com>
2022-12-09 19:42:44 +0100

    Add solution for day 6, star 2

--- day-06/src/main.rs
+++ day-06/src/main.rs
@@ -25,23 +25,23 @@ fn main() {
 
 fn star1(input: &str) -> usize {
     let parsed_input = parse_input(input);
-    get_marker_location(&parsed_input).unwrap()
+    get_marker_location(&parsed_input, 4).unwrap()
 }
 
-fn star2(input: &str) -> i32 {
+fn star2(input: &str) -> usize {
     let parsed_input = parse_input(input);
-    parsed_input.len() as i32
+    get_marker_location(&parsed_input, 14).unwrap()
 }
 
 fn parse_input(input: &str) -> Vec<char> {
     input.trim().chars().collect()
 }
 
-fn get_marker_location(signal: &[char]) -> Option<usize> {
+fn get_marker_location(signal: &[char], marker_length: usize) -> Option<usize> {
     let mut signal_iter_end = signal.iter(); // End of marker
     let mut recent_chars: HashMap<char, usize> = HashMap::new();
-    // Insert first three chars
-    for _ in 0..3 {
+    // Insert first (three) chars
+    for _ in 0..(marker_length - 1) {
         let current_char = signal_iter_end.next().unwrap();
         add_single_char_to_recent_chars(*current_char, &mut recent_chars);
     }
@@ -49,10 +49,9 @@ fn get_marker_location(signal: &[char]) -> Option<usize> {
     let mut signal_iter_start = signal.iter(); // Start of marker
     for (idx, current_char) in signal_iter_end.enumerate() {
         add_single_char_to_recent_chars(*current_char, &mut recent_chars);
-        if recent_chars.len() == 4 {
-            // +3 to compensate for the first three chars
-            // +1 to compensate starting to count with 0
-            return Some(idx + 4);
+        if recent_chars.len() == marker_length {
+            // compensate for the first three chars and starting to count with 0
+            return Some(idx + marker_length);
         }
         // Remove the oldest char from the map
         let oldest_char = signal_iter_start.next().unwrap();
@@ -115,18 +114,37 @@ mod tests {
     #[test]
     fn full_star1() {
         let result = star1(INPUT);
-        assert_eq!(result, 0);
+        assert_eq!(result, 1802);
     }
 
     #[test]
-    fn test_star2() {
+    fn test1_star2() {
         let result = star2(TEST_INPUT_1);
-        assert_eq!(result, 0);
+        assert_eq!(result, 19);
     }
-
+    #[test]
+    fn test2_star2() {
+        let result = star2(TEST_INPUT_2);
+        assert_eq!(result, 23);
+    }
+    #[test]
+    fn test3_star2() {
+        let result = star2(TEST_INPUT_3);
+        assert_eq!(result, 23);
+    }
+    #[test]
+    fn test4_star2() {
+        let result = star2(TEST_INPUT_4);
+        assert_eq!(result, 29);
+    }
+    #[test]
+    fn test5_star2() {
+        let result = star2(TEST_INPUT_5);
+        assert_eq!(result, 26);
+    }
     #[test]
     fn full_star2() {
         let result = star2(INPUT);
-        assert_eq!(result, 0);
+        assert_eq!(result, 3551);
     }
 }
