bff2fbe7b802f94f9c7fef69d0f9927f27b5ba38
Karin Frlic <karin.frlic@gmail.com>
2022-12-10 20:04:01 +0100

    Add solution for day 8, star 2

--- day-08/src/main.rs
+++ day-08/src/main.rs
@@ -20,12 +20,12 @@ fn star1(input: &str) -> usize {
     let tree_grid = parse_input(input);
     let mut visibility_grid = create_visibility_grid(tree_grid.len(), tree_grid[0].len());
     mark_visible_trees(&tree_grid, &mut visibility_grid);
-    count_visible_trees(&visibility_grid)
+    count_visible_trees_from_outside(&visibility_grid)
 }
 
-fn star2(input: &str) -> i32 {
-    let parsed_input = parse_input(input);
-    parsed_input.len() as i32
+fn star2(input: &str) -> usize {
+    let tree_grid = parse_input(input);
+    get_max_score_tree(&tree_grid)
 }
 
 fn parse_input(input: &str) -> TreeGrid {
@@ -101,7 +101,7 @@ fn mark_visible_trees_from_bottom(tree_grid: &TreeGrid, visibility_grid: &mut Vi
     for col_idx in 0..tree_grid[0].len() {
         visibility_grid[tree_grid.len() - 1][col_idx] = true;
         let mut last_visible_height = tree_grid[tree_grid.len() - 1][col_idx];
-        for row_idx in (0..tree_grid.len() - 1).rev() {
+        for row_idx in (0..tree_grid.len()).rev() {
             let tree_height = tree_grid[row_idx][col_idx];
             if tree_height > last_visible_height {
                 visibility_grid[row_idx][col_idx] = true;
@@ -111,11 +111,68 @@ fn mark_visible_trees_from_bottom(tree_grid: &TreeGrid, visibility_grid: &mut Vi
     }
 }
 
-fn count_visible_trees(visibility_grid: &VisibilityGrid) -> usize {
+fn count_visible_trees_from_outside(visibility_grid: &VisibilityGrid) -> usize {
     visibility_grid
         .iter()
         .map(|row| row.iter().filter(|&&visibile| visibile).count())
         .sum()
+}
+
+fn get_max_score_tree(tree_grid: &TreeGrid) -> usize {
+    let mut best_score = 0;
+    for row_idx in 0..tree_grid.len() {
+        for col_idx in 0..tree_grid[0].len() {
+            let score = get_tree_score(row_idx, col_idx, tree_grid);
+            if score > best_score {
+                best_score = score;
+            }
+        }
+    }
+    best_score
+}
+
+fn get_tree_score(row_idx: usize, col_idx: usize, tree_grid: &TreeGrid) -> usize {
+    let ego_tree_height = tree_grid[row_idx][col_idx];
+    let grid_height = tree_grid.len();
+    let grid_width = tree_grid[0].len();
+
+    // left
+    let mut count_left = 0;
+    for idx in (0..col_idx).rev() {
+        count_left += 1;
+        if tree_grid[row_idx][idx] >= ego_tree_height {
+            break;
+        }
+    }
+
+    // right
+    let mut count_right = 0;
+    for idx in col_idx + 1..grid_width {
+        count_right += 1;
+        if tree_grid[row_idx][idx] >= ego_tree_height {
+            break;
+        }
+    }
+
+    // up
+    let mut count_up = 0;
+    for idx in (0..row_idx).rev() {
+        count_up += 1;
+        if tree_grid[idx][col_idx] >= ego_tree_height {
+            break;
+        }
+    }
+
+    // down
+    let mut count_down = 0;
+    for idx in row_idx + 1..grid_height {
+        count_down += 1;
+        if tree_grid[idx][col_idx] >= ego_tree_height {
+            break;
+        }
+    }
+
+    count_left * count_right * count_up * count_down
 }
 
 #[cfg(test)]
@@ -137,12 +194,12 @@ mod tests {
     #[test]
     fn test_star2() {
         let result = star2(TEST_INPUT);
-        assert_eq!(result, 0);
+        assert_eq!(result, 8);
     }
 
     #[test]
     fn full_star2() {
         let result = star2(INPUT);
-        assert_eq!(result, 0);
+        assert_eq!(result, 301392);
     }
 }
